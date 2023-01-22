1eb48deab0285df00e8f53b84c8daf5d38d5b1ea
AbrarShahriar <AbrarShahriar321@gmail.com>
2023-01-22 19:35:38 +0600

    onesignal rest api integrated

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import { PlaywrightCrawler } from "crawlee";
 import { readFile, writeFile } from "fs/promises";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 let price;
+let sendNotificationSwitch = true;
+let call = 1;
 
 const crawler = new PlaywrightCrawler({
   requestHandler: async ({ request, page, log }) => {
@@ -64,12 +69,60 @@ app.get("/", async (req, res) => {
   }
 });
 
+app.get("/notification", async (req, res) => {
+  let rawData = await readFile("./src/data.json", { encoding: "utf-8" });
+  let data = JSON.parse(rawData);
+  let threshold = data.threshold;
+
+  sendNotificationSwitch && (await crawler.run(["https://goldprice.org/"]));
+
+  let goldPrice = price ? parsedPrice() : 0;
+
+  console.log(goldPrice, threshold);
+
+  if (goldPrice !== 0 && goldPrice > threshold) {
+    if (sendNotificationSwitch) {
+      let res = await fetch("https://onesignal.com/api/v1/notifications", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          accept: "application/json",
+          Authorization: `Basic ${process.env.ONESIGNAL_API_KEY}`,
+        },
+        body: JSON.stringify({
+          app_id: process.env.ONESIGNAL_APP_ID,
+          included_segments: ["Subscribed Users"],
+          contents: {
+            en: `Gold Price is now ${goldPrice} which is above ${threshold}`,
+          },
+          headings: {
+            en: `Gold Price Above Threshold!`,
+          },
+          name: "rest_api",
+        }),
+      });
+
+      let data = await res.json();
+
+      console.log(data);
+
+      sendNotificationSwitch = false;
+    }
+  }
+
+  res.status(200).json({ message: `Called ${call} time(s)` });
+
+  call++;
+});
+
 app.post("/", async (req, res) => {
   var body = req.body;
 
   await writeFile("./src/data.json", JSON.stringify(body, null, 2), {
     encoding: "utf-8",
   });
+
+  sendNotificationSwitch = true;
 
   res.status(201).json({ message: "threshold updated" });
 });
